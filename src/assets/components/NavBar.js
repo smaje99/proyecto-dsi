38424f51157a5aaebc16bd9505b5f2ef622f2e21
Sergio Majé <smajefranco@gmail.com>
2021-02-28 14:15:03 -0500

    Indicador de la ruta actual en la barra de navegación

--- src/assets/components/NavBar.js
+++ src/assets/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
@@ -6,24 +6,35 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import '../styles/NavBar.css'
 
 export const NavBar = () => {
+    const [location, setLocation] = useState('inicio');
+
     return (
         <nav className="navbar">
             <Link className="nav-logo" to="/">
                 <img src="img/seros.svg" alt="Logotipo SEROS" />
             </Link>
             <ul className="navbar-menu">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/">
+                <li className={ location === 'inicio' ? 'selected' : ''}>
+                    <Link
+                        className="nav-link"
+                        to="/"
+                        onClick={ e => setLocation('inicio') }>
                         Inicio
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/notas">
+                <li className={ location === 'notas' ? 'selected' : ''}>
+                    <Link
+                        className="nav-link"
+                        to="/notas"
+                        onClick={ e => setLocation('notas') }>
                         Notas
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/acerca">
+                <li className={ location === 'acerca' ? 'selected' : ''}>
+                <Link
+                        className="nav-link"
+                        to="/acerca"
+                        onClick={ e => setLocation('acerca') }>
                         Acerca de
                     </Link>
                 </li>
